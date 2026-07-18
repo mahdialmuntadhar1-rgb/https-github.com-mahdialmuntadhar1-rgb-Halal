e0a77f6d0bb435f8e756957e0f3080a4b5413afd
Mahdi <your-email@example.com>
2026-07-18 10:35:28 +0300

    feat: prepare Zawaj for Android with Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.kaniq.zawaj',
+  appName: 'Zawaj',
+  webDir: 'dist'
+};
+
+export default config;
